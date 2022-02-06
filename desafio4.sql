035436f200a8ff20098030228075fdf5a5ff0b27
Lucas Maieski <lucmaieski@gmail.com>
2022-02-06 19:31:51 -0300

    Feito requisito 4

---
+++ desafio4.sql
@@ -0,0 +1,9 @@
+SELECT u.usuario AS 'usuario', IF(MAX(h.data_reproducao) LIKE '2021%', 'Usuário ativo', 'Usuário inativo' )  AS 'condicao_usuario'
+	FROM 
+		SpotifyClone.Usuario AS u
+	INNER JOIN
+		SpotifyClone.historico_de_reproducoes AS h
+	ON
+		u.usuario_id = h.usuario_id
+	GROUP BY u.usuario
+	ORDER BY u.usuario;
