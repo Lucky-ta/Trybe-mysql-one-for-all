f5d62b13c215be70050219782c3337f111be47a3
Lucas Maieski <lucmaieski@gmail.com>
2022-02-06 02:24:18 -0300

    Feito requisito 2

---
+++ desafio2.sql
@@ -0,0 +1,2 @@
+SELECT COUNT(DISTINCT cancao) AS cancoes, COUNT( DISTINCT artista) AS artistas, COUNT( DISTINCT album) AS albuns
+FROM (SpotifyClone.cancao, SpotifyClone.Artista, SpotifyClone.Album);
